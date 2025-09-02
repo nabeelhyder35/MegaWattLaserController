a9a7afdd3d4fe4cb9e46e8b3bd6dab3e49d66bbe
nabeel.hyder <nabeel.hyder@anchoragesource.com>
2025-09-02 14:45:34 +0500

    v2

--- MegaWattLaserController/obj/x64/Debug/net8.0-windows10.0.19041.0/LaserControllerApp.AssemblyInfo.cs
+++ MegaWattLaserController/obj/x64/Debug/net8.0-windows10.0.19041.0/LaserControllerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaserControllerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91603d7f264675eca6a4d01174287d1029aa1638")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21ac93918cf8b6dd1b52f77c8c7c454700aa552c")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaserControllerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaserControllerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
